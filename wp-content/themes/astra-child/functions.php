60fb70a7fa5f28a672c4e68773c4ab19b1c16b99
Tang Edgetech <tang.edgetech@gmail.com>
2026-04-08 14:40:00 +0800

    Update functions.php

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -35,6 +35,6 @@ function child_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 function year() {
-    $year = new Date('Y');
+    $year = date('Y');
     return $year;
 }
