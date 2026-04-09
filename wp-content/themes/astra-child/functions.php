d29784af76eb6f839c5a68ae6075a1ff53d5cd70
Tang Edgetech <tang.edgetech@gmail.com>
2026-04-09 11:46:47 +0800

    Update navigation v6

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -61,7 +61,7 @@ function hajimi_search_form_function() {
     <div class="hajimi-searchbar">
         <button type="button" class="btn btn-search hajimi-searchbar-button d-flex d-md-none"><i class="fas fa-search" aria-hidden="true"></i><span class="d-none">Search</span></button>
         <form class="hajimi-search-form" method="get" action="/">
-            <div class="form-inner d-flex">
+            <div class="form-inner d-flex justify-content-center">
                 <input type="text" id="hajimi-search" name="s" value="<?php the_search_query();?>" placeholder="Search Libraries"/>
                 <button type="submit" class="btn btn-search hajimi-search-form-button"><i class="fas fa-search" aria-hidden="true"></i><span class="d-none">Search</span></button>
             </div>
