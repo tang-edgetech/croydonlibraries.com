e3d0bc135ce601a799d9f23f7ad809258f5bbd5b
Tang Edgetech <tang.edgetech@gmail.com>
2026-04-09 21:34:05 +0800

    Closing 9th April

--- wp-content/themes/astra-child/page.php
+++ wp-content/themes/astra-child/page.php
@@ -6,15 +6,19 @@ get_header();
     <?php
     while (have_posts()) {
         the_post();
-        if ( !is_front_page() && !is_search() && !is_404() ) {
-        ?>
-        <div class="hajimi-container">
-            <div class="hajimi-page-header">
-                <div class="hajimi-page-title"><?= get_the_title();?></div>
+        if ( !is_front_page() || !is_search() || !is_404() ) {
+            $first_model = ['join-the-library', 'find-the-library', 'online-library', 'news-events'];
+            $current_slug = get_post_field('post_name', get_post());
+            if (in_array($current_slug, $first_model)) {
+            ?>
+            <div class="hajimi-container">
+                <div class="hajimi-page-header">
+                    <div class="hajimi-page-title"><?= get_the_title();?></div>
+                </div>
+                <?php hajimi_breadcrumb();?>
             </div>
-            <?php hajimi_breadcrumb();?>
-        </div>
         <?php
+            }
         }
         the_content();
     }
