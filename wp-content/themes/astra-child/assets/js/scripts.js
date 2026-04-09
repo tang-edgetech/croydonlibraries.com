3338327d8da1b20079c6d55a8679a458fb06b528
Tang Edgetech <tang.edgetech@gmail.com>
2026-04-09 10:40:35 +0800

    Update mobile navigation

--- wp-content/themes/astra-child/assets/js/scripts.js
+++ wp-content/themes/astra-child/assets/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    function HajimiOnResize() {
+        if( window.matchMedia("(max-width: 1199px)").matches ) {
+            $(document).on('click', '#masthead .elementor-widget-eael-simple-menu .eael-simple-menu-toggle', function() {
+                var $button = $(this);
+                $button.toggleClass('is-active');
+            })
+        }
+        else {
+            $('#masthead .elementor-widget-eael-simple-menu .eael-simple-menu-toggle').removeClass('is-active');
+        }
+    }
+    $(window).on('resize', HajimiOnResize);
+    HajimiOnResize();
 });
