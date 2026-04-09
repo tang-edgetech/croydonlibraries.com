7ae11b2ec62c20e52cc074b374f2b8bacabed7bd
Tang Edgetech <tang.edgetech@gmail.com>
2026-04-09 11:51:04 +0800

    Update navigation v7

--- wp-content/themes/astra-child/assets/js/scripts.js
+++ wp-content/themes/astra-child/assets/js/scripts.js
@@ -4,10 +4,27 @@ $(document).ready(function() {
             $(document).on('click', '#masthead .elementor-widget-eael-simple-menu .eael-simple-menu-toggle', function() {
                 var $button = $(this);
                 $button.toggleClass('is-active');
-            })
+            });
+
+            $(document).on('click', '.hajimi-searchbar > .btn.btn-search', function() {
+                var $button = $(this),
+                    $parent = $button.parents('.hajimi-searchbar');
+                if( $('.hajimi-searchbar').hasClass('searchbar-opened') ) {
+                    $('.hajimi-searchbar').removeClass('searchbar-opened');
+                    $('.hajimi-searchbar .hajimi-search-form').fadeOut();
+                }
+                else {
+                    $('.hajimi-searchbar').addClass('searchbar-opened');
+                    $('.hajimi-searchbar .hajimi-search-form').fadeIn();
+                }
+            });
         }
         else {
             $('#masthead .elementor-widget-eael-simple-menu .eael-simple-menu-toggle').removeClass('is-active');
+            if( $('.hajimi-searchbar').hasClass('searchbar-opened') ) {
+                $('.hajimi-searchbar').removeClass('searchbar-opened');
+                $('.hajimi-searchbar .hajimi-search-form').fadeOut();
+            }
         }
     }
     $(window).on('resize', HajimiOnResize);
